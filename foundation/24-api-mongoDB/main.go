3d81c0f216eeb79e128febf763909ce2eee627e3
Mehdi Hasan  Shohan <mehdihasanshohan25@gmail.com>
2024-08-27 23:51:19 +0600

    go mongoDB api initialized

---
+++ foundation/24-api-mongoDB/main.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+	// connect to mongoDB
+	// create a collection
+	// insert a document
+	// find a document
+	// update a document
+	// delete a document
+	// close the connection
+}
