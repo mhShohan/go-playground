5670a8d85277150f9d56c684f158b93d8a581506
mhShohan <mehdihasanshohan25@gmail.com>
2024-09-25 19:13:03 +0600

    basic math using go lang

--- learning-dsa/step_1---the-basic/basic-maths/main.go
+++ learning-dsa/step_1---the-basic/basic-maths/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"sort"
 )
 
 func countDigit(n int) {
@@ -46,10 +47,76 @@ func reverseNumber(n int) {
 	fmt.Println(reverseNum * sign)
 }
 
+func checkPalindrome(n int) {
+	N := n
+	reverseNumber := 0
+
+	for N > 0 {
+		lastDigit := N % 10
+		N = int(N / 10)
+
+		reverseNumber = (reverseNumber * 10) + lastDigit
+	}
+
+	fmt.Println(reverseNumber == n)
+}
+
+func armstrongNumber(n int) {
+	N := n
+	result := 0
+	for N > 0 {
+		lastDigit := N % 10
+		N = int(N / 10)
+
+		result += lastDigit * lastDigit * lastDigit
+	}
+
+	fmt.Println(n, "=>", result == n)
+}
+
+func printAllDivisors(n int) {
+	divisors := []int{}
+
+	for i := 1; i <= n; i++ {
+		if n%i == 0 {
+			divisors = append(divisors, i)
+		}
+	}
+
+	fmt.Println(divisors)
+	// time complexity: O(n)
+}
+
+func printAllDivisorsOptimized(n int) {
+	divisors := []int{}
+
+	for i := 1; i <= int(math.Sqrt(float64(n))); i++ {
+		if n%i == 0 {
+			divisors = append(divisors, i)
+
+			if i != n/i {
+				divisors = append(divisors, n/i)
+			}
+		}
+	}
+
+	sort.Ints(divisors)
+
+	fmt.Println(divisors)
+
+	// time complexity: O(sqrt(n))
+}
+
 func main() {
 	countDigit(23453345)
 	countDigitOptimized(23453345)
 	reverseNumber(-12345)
 	reverseNumber(12345)
 
+	checkPalindrome(12321)
+	checkPalindrome(124241)
+	armstrongNumber(371)
+	armstrongNumber(5434)
+	printAllDivisors(24)
+	printAllDivisorsOptimized(36)
 }
